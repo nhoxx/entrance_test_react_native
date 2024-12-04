c882dd6acb6a04d4e101c241d1cfdbf9bcaa93f8
Nguyen.Nguyen <threeng0227@gmail.com>
2024-12-04 16:29:24 +0700

    update

--- src/navigation/NavigationStack.tsx
+++ src/navigation/NavigationStack.tsx
@@ -1,39 +1,38 @@
-import React, { useEffect } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { navigationRef } from './NavigationService'
-import { StatusBar } from 'react-native'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { Routes } from './Routes'
-import { SingUpScreen } from '../features/sign-up/SignUp'
-import { CategoriesScreen } from '../features/categorites/Categories'
-import ModalLoading from '../components/ModalLoading'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { navigationRef } from "./NavigationService";
+import { StatusBar } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Routes } from "./Routes";
+import { SingUpScreen } from "../features/sign-up/SignUp";
+import { CategoriesScreen } from "../features/categorites/Categories";
+import ModalLoading from "../components/ModalLoading";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export const StackNavigator = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <>
-        <StatusBar
-          barStyle={'light-content'}
-          translucent
-        />
+        <StatusBar barStyle={"light-content"} translucent />
         <Stack.Navigator
           initialRouteName={Routes.SignUp}
           screenOptions={{
             headerShown: false,
-            animation: 'slide_from_right',
+            animation: "slide_from_right",
           }}
         >
           <Stack.Screen name={Routes.SignUp} component={SingUpScreen} />
-          <Stack.Screen name={Routes.Categorires} component={CategoriesScreen} />
+          <Stack.Screen
+            name={Routes.Categorires}
+            component={CategoriesScreen}
+          />
         </Stack.Navigator>
       </>
     </GestureHandlerRootView>
-  )
-}
+  );
+};
 
 export const AppNavigator = () => {
   return (
@@ -41,5 +40,5 @@ export const AppNavigator = () => {
       <StackNavigator />
       <ModalLoading />
     </NavigationContainer>
-  )
-}
+  );
+};
